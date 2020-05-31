16b638a249e67fce8b825d735004f9f45157df30
Jassan <jassan@rfgsoftware.com>
2020-05-30 22:40:25 -0700

    Player movement and platform collision handling

---
+++ Assets/Platform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.y <= 0.0f)
+        {
+            Rigidbody2D playerRB = collision.collider.GetComponent<Rigidbody2D>();
+
+            if (playerRB)
+            {
+                Vector2 velocity = playerRB.velocity;
+                velocity.y = jumpForce;
+                playerRB.velocity = velocity;
+
+            }
+        }
+    }
+
+
+}

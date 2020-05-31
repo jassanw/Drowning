0654e4b6b63d08b10f5e87ab85484aa5de7a65ca
Georges Nasrallah <georges19232@gmail.com>
2020-05-31 00:06:31 -0700

    added camera movements to the player

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform playerTransform;
+
+    public float smoothSpeed = 0.2f;
+    void LateUpdate()
+    {
+        if (playerTransform.position.y > transform.position.y)
+        {
+            Vector3 desiredPosition = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+            // transform.LookAt(playerTransform);
+        }
+    }
+}

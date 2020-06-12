96894fd895078e01e6d9d0c22c67435dd8019e85
Georges Nasrallah <georges19232@gmail.com>
2020-06-11 22:35:19 -0700

    Added the water block along with a script to make the water rise per frame

---
+++ Assets/WaterRising.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterRising : MonoBehaviour
+{
+    public float risingSpeed = 0.005f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPosition = transform.position;
+
+        var newWaterPositionY = currentPosition.y + risingSpeed;
+
+        Vector3 newWaterPosition = new Vector3(currentPosition.x, newWaterPositionY, currentPosition.z);
+        transform.position = newWaterPosition;
+    }
+}

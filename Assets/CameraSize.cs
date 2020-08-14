eb88fdf962cd0ac945926ce2034e8272993e430a
Adam Miller <adamtm777@gmail.com>
2020-08-13 23:11:09 -0700

    Camera size fixed to width

---
+++ Assets/CameraSize.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSize : MonoBehaviour
+{
+
+    public float ScreenWidth = 6;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.orthographicSize = 1/cam.aspect*ScreenWidth/2;
+    }
+}

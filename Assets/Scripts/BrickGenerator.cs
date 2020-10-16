2de363e96cfe95c834f623ee7f717138ed376c7a
unknown <georges19232@gmail.com>
2020-10-16 15:11:07 -0700

    moved around prefabs and improved the generator

--- Assets/Scripts/BrickGenerator.cs
+++ Assets/Scripts/BrickGenerator.cs
@@ -4,28 +4,25 @@ using UnityEngine;
 
 public class BrickGenerator : MonoBehaviour
 {
-
     public float minYHeight = 0.4f;
     public float maxYHeight = 10.0f;
-    public GameObject brick;
+    private float maxXWidth = 2f;
+    private float minXWidth = -2f;
+
+    public  List<GameObject> bricks;
     public GameObject intialBrick;
     public GameObject player;
     public GameObject BrickContainer;
 
-
     private float currentMaxBrickHeight;
-    private float maxXWidth = 2f;
-    private float minXWidth = -2f;
     private Vector3 spawnPosition;
     private GameObject newBrick;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
         spawnPosition = intialBrick.transform.position;
-        generateBricks();
+        GenerateBricks();
     }
 
     // Update is called once per frame
@@ -33,23 +30,39 @@ public class BrickGenerator : MonoBehaviour
     {
         if (currentMaxBrickHeight - player.transform.position.y < 20)
         {
-            generateBricks();
+            GenerateBricks();
         }
     }
 
-    private void generateBricks()
+    private void GenerateBricks()
     {
         for (int i = 0; i < 30; i++)
         {
             spawnPosition.y += Random.Range(minYHeight, maxYHeight);
             spawnPosition.x = Random.Range(minXWidth, maxXWidth);
-            newBrick = Instantiate(brick, spawnPosition, Quaternion.identity);
+
+            newBrick = Instantiate(
+                bricks[Random.Range(0, bricks.Count)], 
+                spawnPosition, 
+                Quaternion.Euler(0, Rotation(), 0)
+            );
             newBrick.transform.parent = BrickContainer.transform;
+
             if (i == 29)
             {
                 currentMaxBrickHeight = spawnPosition.y;
             }
         }
+    }
 
+    private int Rotation() 
+    {
+        var rotation = 0;
+        if (Random.value < 0.5f) 
+        {
+            rotation = 180;
+        }    
+
+        return rotation;
     }
 }

6d6cc366b11e8331d1dcb788f422b83f18e4b4f4
Jassan <jassan@rfgsoftware.com>
2021-07-13 09:36:44 -0700

    reset

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class Enums
+{
+    public enum Items
+    {
+        JumpyBoots,
+        FlyingFeather,
+        Web
+    }
+
+    public static Items GetRandomItemEnum()
+    {
+        return Enum.GetValues(typeof(Items))
+             .OfType<Items>()
+             .OrderBy(e => Guid.NewGuid())
+             .FirstOrDefault();
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public float tiltMultiplier = 3f;
+    public float jumpForce = 10f;
+    
+    public Rigidbody2D playerRigidBody;
+    float movement = 0.0f;
+    float direction;
+    bool _isSlowingDown;
+    bool _playerFrozen = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            movement = Input.GetAxis("Horizontal") * movementSpeed;
+            if (Input.acceleration.x != 0)
+            {
+                movement = Input.acceleration.x * movementSpeed * tiltMultiplier;
+            }
+
+            if (movement > 1 || movement < -1)
+            {
+                direction = movement / Math.Abs(movement);
+                playerRigidBody.transform.localScale = new Vector3(
+                    direction * Math.Abs(playerRigidBody.transform.localScale.x),
+                    playerRigidBody.transform.localScale.y,
+                    0
+                );
+            }
+    }
+
+ 
+    private void FixedUpdate()
+    {
+        Vector2 velocity = playerRigidBody.velocity;
+        velocity.x = movement;
+        playerRigidBody.velocity = velocity;
+        if (_isSlowingDown)
+        {
+            playerRigidBody.velocity = playerRigidBody.velocity * 0.98f * Time.fixedDeltaTime;
+        }
+    }
+
+    public Vector3 GetPosition(){
+        return transform.position;
+    }
+
+    public void SetMovementSpeed(float movementSpeed)
+    {
+        this.movementSpeed = movementSpeed;
+    }
+
+    public void DeactivatePlayer() {
+        gameObject.SetActive(false);
+    }
+
+    public void ResetPlayer()
+    {
+        movementSpeed = 10f;
+        jumpForce = 10f;
+        playerRigidBody.gravityScale = 1;
+        playerRigidBody.mass = 1;
+        _isSlowingDown = false;
+        playerRigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    public void SetPlayerFrozen(bool frozen)
+    {
+        _playerFrozen = frozen;
+    }
+
+    public void OnPlatformJump()
+    {
+        Vector2 velocity = playerRigidBody.velocity;
+        velocity.y = jumpForce;
+        playerRigidBody.velocity = velocity;
+    }
+
+
+
+}
